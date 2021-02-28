b850e4f8a9921609997e1054d0592e180cc413e1
Pragya <pjilowa@ce.iitr.ac.in>
2021-02-28 20:44:19 +0530

    Data fetching and processing from HERE map intermodular-routing api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
+const getTravelData = require("../views/travelData");
 const pollutionViews = require("../views/pollution");
 
 router.get("/aqi/:id/", pollutionViews.getPollutionData);
+
+router.get("/gettraveldata/:query", getTravelData);
 
 module.exports = router;
---
+++ views/travelData.js
@@ -0,0 +1,67 @@
+const axios = require("axios");
+const querystring = require("querystring");
+const moment = require("moment");
+const API_URL = "https://intermodal.router.hereapi.com/v8/routes";
+
+function getTravelData(req, res) {
+  let queryStr = req.params.query;
+  let queryObj = querystring.parse(queryStr);
+  let origin = queryObj.origin;
+  let dest = queryObj.dest;
+
+  let p = axios.get(API_URL, {
+    params: {
+      apiKey: process.env.api_key,
+      alternatives: 0,
+      destination: dest,
+      origin: origin,
+    },
+  });
+  p.then((response) => {
+    let result = formatData(response.data.routes[0]);
+    res.send(result);
+  }).catch((error) => {
+    if (error.response) {
+      if (error.response.status == 400) {
+        res.json({ msg: error.response.title }).status(400);
+      } else if (error.response.status == 401) {
+        res
+          .json({ msg: "error while authorisation to the HERE map server" })
+          .status(401);
+      } else {
+        res
+          .json({
+            msg: "Unknown error occured while fetching the data from server",
+          })
+          .status(error.response.status);
+      }
+    } else if (error.request) {
+      res
+        .json({ msg: "The request was made but no response was received" })
+        .status(500);
+    } else {
+      res.json({ msg: "Internal server error" }).status(500);
+    }
+  });
+}
+function formatData(route) {
+  let departurePlace = route.sections[0].departure.place;
+  let arrivalPlace = route.sections[route.sections.length - 1].arrival.place;
+  let startTime = route.sections[0].departure.time;
+  let endTime = route.sections[route.sections.length - 1].arrival.time;
+  let timeTravelled = moment
+    .duration(
+      moment(endTime, "YYYY/MM/DD HH:mm").diff(
+        moment(startTime, "YYYY/MM/DD HH:mm")
+      )
+    )
+    .asSeconds();
+  //In seconds
+  return {
+    timeTravelled,
+    departurePlace,
+    arrivalPlace,
+  };
+}
+
+module.exports = getTravelData;
