2bb6d333ff06d7c1d8643005d1660884e8099b35
Pragya <pjilowa@ce.iitr.ac.in>
2021-05-01 09:12:24 +0530

    calculated congestion value with the help of a here map API

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-
+var bodyParser = require("body-parser");
 const routes = require("./routes");
 
 require("dotenv").config();
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
-
+app.use(bodyParser.json());
 app.use("/", routes);
 
 app.get("/", (req, res) => res.json({ msg: "API initialized" }).status(200));
-
+app.use((req, res) => {
+  res
+    .status(400)
+    .json({
+      msg: "The endpoint you are looking for is not there! Have a good day:)",
+    });
+});
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const getTravelData = require("../views/travelData");
 const pollutionViews = require("../views/pollution");
+const calculateCongestion = require("../views/congestion");
 
 router.get("/aqi/:id/", pollutionViews.getPollutionData);
 
 router.get("/gettraveldata/:query", getTravelData);
+router.post("/getCongestion", calculateCongestion);
 
 module.exports = router;
